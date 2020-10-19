9c5e72f7d315d034f9136764270e7c9c429793a2
pcapriles <pedro.capriles@walmart.com>
2020-10-19 18:12:26 -0300

    Configuration project base

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { config } = require('./config');
+const moviesApi = require('./src/routes/movies');
+const { logErrors, errorHandler, wrapErrors } = require('./src/middleware/errorHandler');
+const notFoundHandler = require('./src/middleware/notFoundHandler');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(helmet());
+
+moviesApi(app);
+app.use(notFoundHandler);
+
+// Errors managment middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+app.listen(config.port, () => {
+  console.log(`Listening http://localhost:${config.port} ✅`);
+});
