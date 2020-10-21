830f3a6c60281fad04883cffdf767adb303a1ad0
pcapriles <pedro.capriles@walmart.com>
2020-10-21 09:00:30 -0300

    Add test and fix bugs

--- test/GildedRose/GildedRose.test.js
+++ test/GildedRose/GildedRose.test.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-undef */
 const Item = require('../../src/GildedRose/Item');
 const GildedRose = require('../../src/GildedRose/GildedRose');
-const { expectationFailed } = require('@hapi/boom');
 describe('Test suite for GildedRose', () => {
 
     const arrayWith = (item) => {
@@ -40,5 +40,80 @@ describe('Test suite for GildedRose', () => {
         gildedRose.updateQuality();
 
         expect(whateverItem.quality).toBe(0);
+    });
+
+    test('Should test Aged Brie Increases quality with age', () => {
+        const agedBrie = new Item('Aged Brie', 5, 1);
+
+        const gildedRose = new GildedRose(arrayWith(agedBrie));
+        gildedRose.updateQuality();
+
+        expect(agedBrie.quality).toBe(2);
+    });
+
+    test('Should test quality never increases past fifty', () => {
+        const agedBrie = new Item('Aged Brie', 5, 50);
+
+        const gildedRose = new GildedRose(arrayWith(agedBrie));
+        gildedRose.updateQuality();
+
+        expect(agedBrie.quality).toBe(50);
+    });
+
+    test('Should sulfurus never changes', () => {
+        const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 25);
+
+        const gildedRose = new GildedRose(arrayWith(sulfuras));
+        gildedRose.updateQuality();
+
+        expect(sulfuras.quality).toBe(25);
+        expect(sulfuras.sellIn).toBe(0);
+    });
+
+    test('Should sulfurus never changes', () => {
+        const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 25);
+
+        const gildedRose = new GildedRose(arrayWith(sulfuras));
+        gildedRose.updateQuality();
+
+        expect(sulfuras.quality).toBe(25);
+        expect(sulfuras.sellIn).toBe(0);
+    });
+
+    test('Should sulfurus never changes', () => {
+        const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 25);
+
+        const gildedRose = new GildedRose(arrayWith(sulfuras));
+        gildedRose.updateQuality();
+
+        expect(sulfuras.quality).toBe(25);
+        expect(sulfuras.sellIn).toBe(0);
+    });
+
+    test('Should Backstagepass Increases Quality by One if sell by greater then ten', () => {
+        const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20);
+
+        const gildedRose = new GildedRose(arrayWith(backstagePasses));
+        gildedRose.updateQuality();
+
+        expect(backstagePasses.quality).toBe(21);
+    });
+
+    test('Should Backstagepass Increases Quality by One if sell by greater then ten', () => {
+        const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 6, 20);
+
+        const gildedRose = new GildedRose(arrayWith(backstagePasses));
+        gildedRose.updateQuality();
+
+        expect(backstagePasses.quality).toBe(22);
+    });
+
+    test('Should Backstagepass Increases quali', () => {
+        const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 6, 20);
+
+        const gildedRose = new GildedRose(arrayWith(backstagePasses));
+        gildedRose.updateQuality();
+
+        expect(backstagePasses.quality).toBe(22);
     });
 })
