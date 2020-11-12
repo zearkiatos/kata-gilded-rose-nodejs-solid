99d97f991baae388ca6abc869b1652d1feeeb719
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-11-11 22:46:21 -0300

    Create ItemName class

---
+++ src/GildedRose/ItemName.js
@@ -0,0 +1,28 @@
+class ItemName {
+
+    initialization() {
+        this.AGED_BRIE = "Aged Brie";
+        this.BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        this.SULFURAS = "Sulfuras, Hand of Ragnaros";
+        this.value = ''
+    }
+
+    constructor(value) {
+        this.value = value;
+    }
+
+    isAgedBrie() {
+        return this.AGED_BRIE === this.value;
+    }
+
+    isBackstagePasses() {
+        return this.BACKSTAGE_PASSES === this.value;
+    }
+
+    isSulfuras() {
+        return this.SULFURAS === this.value;
+    }
+
+}
+
+export default ItemName;
